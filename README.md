df54b2b5cc329332b1f00301de84ae09907a3567
Jonathan Clem <jonathan@jclem.net>
2014-03-20 09:32:55 -0700

    add options support

--- README.md
+++ README.md
@@ -24,9 +24,36 @@ $ ./node_modules/.bin/heroku-proxy
 ### Require it in your `index.js`:
 
 ```javascript
-require('heroku-proxy');
+var proxy = require('heroku-proxy');
+proxy();
 ```
 
 Now, running `foreman run nodemon index.js` will serve content inside the `public`
 directory and process it with harp.js. Any calls to `/api/*` will be proxied
 through the Heroku API.
+
+## Options
+
+By default, heroku-proxy will use harp middleware and start the server for you.
+You can prevent that with the `harp` and `startServer` options:
+
+```javascript
+var proxy = require('heroku-proxy');
+
+proxy({
+  harp: false,
+  startServer: false
+});
+```
+
+If you'd like to pass in your own express app, you can do that, as well:
+
+```javascript
+var express = require('express');
+var proxy   = require('heroku-proxy');
+var app     = express();
+
+proxy(app, {
+  harp: false
+});
+```
--- index.js
+++ index.js
@@ -7,20 +7,46 @@ var path          = require('path');
 var bouncer       = require('./lib/bouncer');
 var cookieSession = require('./lib/cookie-session');
 var api           = require('./lib/api');
-var publicDir     = path.join(process.cwd(), 'public');
-var app           = express();
 
-app.use(express.cookieParser(process.env.COOKIE_SECRET));
-app.use(cookieSession);
-bouncer(app);
-app.use(express.favicon());
-app.use(express.csrf());
-app.use(express.static(publicDir));
-app.use(harp.mount(publicDir));
+module.exports = function(app, options) {
+  var defaultOptions = {
+    harp       : true,
+    startServer: true,
+    publicDir  : path.join(process.cwd(), 'public')
+  };
 
-app.get('/api/*', api.api);
-app.post('/api/*', api.api);
-app.put('/api/*', api.api);
-app.delete('/api/*', api.api);
+  if (typeof app === 'object') {
+    options = app;
+    app     = null;
+  }
 
-clusterflock(app);
+  for (var key in defaultOptions) {
+    if (!options.hasOwnProperty(key)) {
+      options[key] = defaultOptions[key];
+    }
+  }
+
+  if (!app) {
+    app = express();
+  }
+
+  app.use(express.cookieParser(process.env.COOKIE_SECRET));
+  app.use(cookieSession);
+  bouncer(app);
+  app.use(express.favicon());
+  app.use(express.csrf());
+  app.use(express.static(options.publicDir));
+
+  if (options.harp) {
+    app.use(harp.mount(options.publicDir));
+  }
+
+  app.get('/api/*', api.api);
+  app.post('/api/*', api.api);
+  app.put('/api/*', api.api);
+  app.delete('/api/*', api.api);
+
+  if (options.startServer) {
+    clusterflock(app);
+  }
+};
