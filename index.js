9773a10894e915198b7f3929bef72c392efee08c
Jonathan Clem <jonathan@jclem.net>
2014-03-23 00:35:28 -0700

    add a simplified proxy

--- index.js
+++ index.js
@@ -5,42 +5,26 @@ var express       = require('express');
 var path          = require('path');
 var bouncer       = require('./lib/bouncer');
 var cookieSession = require('./lib/cookie-session');
-var api           = require('./lib/api');
+var proxy         = require('./lib/proxy');
 
 module.exports = function(app, options) {
-  var defaultOptions = {
-    startServer: true,
-    publicDir  : path.join(process.cwd(), 'public')
-  };
-
   options || (options = {});
+  options.startServer || (options.startServer = true);
 
   if (typeof app === 'object') {
     options = app;
     app     = null;
   }
 
-  for (var key in defaultOptions) {
-    if (!options.hasOwnProperty(key)) {
-      options[key] = defaultOptions[key];
-    }
-  }
-
-  if (!app) {
-    app = express();
-  }
+  if (!app) app = express();
 
   app.use(express.cookieParser(process.env.COOKIE_SECRET));
   app.use(cookieSession);
   bouncer(app);
   app.use(express.favicon());
   app.use(express.csrf());
-  app.use(express.static(options.publicDir));
-
-  app.get('/api/*', api.api);
-  app.post('/api/*', api.api);
-  app.put('/api/*', api.api);
-  app.delete('/api/*', api.api);
+  app.use(express.static(path.join(process.cwd(), 'public')));
+  app.all('/api/*', proxy);
 
   if (options.startServer) {
     clusterflock(app);
